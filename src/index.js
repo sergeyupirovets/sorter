df192d6ec3077f41f777db0b6fccfdb0ef53cb2c
Sergey Upirovets <sergey1b.u@gmail.com>
2018-02-26 16:47:33 +0300

    test issue3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
 class Sorter {
   constructor() {
     // your implementation
+     this.elements = [];
   }
 
   add(element) {
     // your implementation
+    this.elements.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.elements[index];
   }
 
   get length() {
     // your implementation
+     return this.elements.length;
   }
 
   toArray() {
     // your implementation
+    return this.elements;
   }
 
   sort(indices) {
     // your implementation
+    let tempArr = [];
+    let tempArr2 = [];
+    let tempArr3 = [];
+    indices.forEach((el) => {
+      if (this.elements[el]) {
+        tempArr.push(this.elements[el]);
+      }
+    });
+    tempArr.sort();
+     for (let i=0; i<indices.length; i++) {
+       this.elements[indices[i]] = tempArr[i]
+     }
+    // this.elements = tempArr;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    //
+    // let tempArr = [];
+    // // your implementation
+    // tempArr.sort();
+    // this.elements.sort();
   }
 }
 
